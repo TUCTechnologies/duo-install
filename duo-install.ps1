31df873d215988e42f4b2a8d7be473de6c3d7c4c
Brandon <brandon@willjasen.com>
2022-05-31 11:27:47 -0400

    adding variables

--- duo-install.ps1
+++ duo-install.ps1
@@ -1,6 +1,11 @@
 $Source = "duo-win-login-4.2.0.exe"
 $Destination = "C:\Windows\Temp\duo-win-login-4.2.0.exe"
 
+# Fill these in from Duo dashboard
+$IKEY = "";
+$SKEY = "";
+$HOST = "";
+
 # If the file isn't in the temp folder, copy it there
 If (-Not (Get-ChildItem -Path "C:\Windows\Temp" -Filter "duo-win-login-4.2.0.exe") ) {
   Copy-Item -Path $Source -Destination $Destination
@@ -13,7 +18,7 @@ $Installed = ((gp HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*).D
 # If app is not installed, install it
 If(-Not $Installed) {
 	Write-Host "'$Software' not installed."
-	Start-Process -Wait -FilePath "$Destination" -ArgumentList '/S', '/V"', '/qn', 'IKEY=""', 'SKEY=""', 'HOST=""', 'AUTOPUSH="#1"', 'FAILOPEN="#1"', 'SMARTCARD="#0"', 'RDPONLY="#0""' -PassThru
+	Start-Process -Wait -FilePath "$Destination" -ArgumentList '/S', '/V"', '/qn', 'IKEY="$IKEY"', 'SKEY="$SKEY"', 'HOST="$HOST"', 'AUTOPUSH="#1"', 'FAILOPEN="#1"', 'SMARTCARD="#0"', 'RDPONLY="#0""' -PassThru
 } else {
 	Write-Host "'$Software' is installed."
 }
